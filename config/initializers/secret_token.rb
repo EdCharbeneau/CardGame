b8098f8d5c3cf2cab20e018a93eed0f1bbeb264e
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-12-21 10:15:30 -0500

    New rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardGame::Application.config.secret_token = '7ca4de5bb72857fcaaa954dfad141cc776171096a15d30027636f4068bc6037f7cb350a50562d75a62c13ff6d9cbcdb2641f392f6f58dfb507d67937a7f79780'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CardGame::Application.config.session_store :cookie_store, key: '_CardGame_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CardGame::Application.config.session_store :active_record_store
