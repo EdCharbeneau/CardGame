b3045a046c4686062847838585d49d7c6cb03c4c
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-12-23 11:50:28 -0500

    Updated game and add game scorer

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,13 +2,14 @@ class Game
   attr_reader :deck, :hands
 
   def initialize(number_of_hands = 4)
+    @number_of_hands = number_of_hands
     @hands = []
-    number_of_hands.times {@hands << Hand.new }
+    @number_of_hands.times {@hands << Hand.new }
     @deck = Deck.new
   end
 
   def start
-    5.times {@hands.each {|hand| @deck.deal_to hand}}
+    @number_of_hands.times {@hands.each {|hand| @deck.deal_to hand}}
   end
 
 end
