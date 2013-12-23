520339dfa683affc1567bc899526525b907ed495
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-12-23 13:52:37 -0500

    Added identifiers to player hands

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,15 +1,27 @@
 class Game
   attr_reader :deck, :hands
 
-  def initialize(number_of_hands = 4)
-    @number_of_hands = number_of_hands
-    @hands = []
-    @number_of_hands.times {@hands << Hand.new }
+  def initialize(names = ["A","B","C","D"])
+    initialize_players(names)
+    initialize_hands
     @deck = Deck.new
   end
 
   def start
+<<<<<<< HEAD
     5.times {@hands.each {|hand| @deck.deal_to hand}}
+=======
+   @hands.each {|hand| @deck.deal_to hand}
+>>>>>>> add-identifers-to-hands
   end
 
+  private
+  def initialize_players(names)
+    @players = Hash.new(0)
+    names.each { |name| @players[SecureRandom.uuid] = name }
+  end
+  def initialize_hands
+    @hands = []
+    @players.keys.each {|player| @hands << Hand.new(player) }
+  end
 end
