561a5bd6e40b731695b93cc40a557c511e68194d
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-12-24 20:36:15 -0500

    Theme complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
-    player_pool = ["Nick Pettit", "Amit Bijlani", "Chris Michel", "Tommy Morgana", "Jason Seifer"]
+    player_pool = ["Nick Pettit", "Amit Bijlani", "Chris Michel", "Tommy Morgan", "Jason Seifer"]
     player_pool.shuffle
-    @players = [player_pool[0],player_pool[1],player_pool[2],"You"]
+    @players = [player_pool[0], player_pool[1], player_pool[2], player_pool[3], "You"]
     @game = Game.new(@players)
     @game.start
     @scorer = GameScorer.new(@game)
