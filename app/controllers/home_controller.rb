26bef2e82516598abbe6b8067d05593f89b4439c
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-12-24 21:38:04 -0500

    Simple game complete
    Things to do
    Refactor
    Add a discard round
    Add betting
    Add AI

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,16 @@
 class HomeController < ApplicationController
   def index
     player_pool = ["Nick Pettit", "Amit Bijlani", "Chris Michel", "Tommy Morgan", "Jason Seifer"]
-    player_pool.shuffle
-    @players = [player_pool[0], player_pool[1], player_pool[2], player_pool[3], "You"]
-    @game = Game.new(@players)
+    player_pool.shuffle!
+    players = [player_pool[0], player_pool[1], player_pool[2], player_pool[3], "You"]
+    @game = Game.new(players)
     @game.start
-    @scorer = GameScorer.new(@game)
+    scorer = GameScorer.new(@game)
+    session[:the_game] = @game
+    session[:the_score] = scorer
+  end
+  def play
+    @scorer = session[:the_score]
+    @game = session[:the_game]
   end
 end
